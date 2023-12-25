b5c970dc9525a16134a436c1d93e191982cc3279
Stepan <legotkinc2006@gmail.com>
2023-12-25 23:34:11 +0500

    Реализованы классы. Леготкин

---
+++ Cinema_Леготкин/Classes/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Леготкин.Classes
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public int idTeatr { get; set; }
+        public string movie { get; set; }
+        public DateTime time { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ Cinema_Леготкин/Classes/Teatr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Леготкин.Classes
+{
+    public class Teatr
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int countZal { get; set; }
+        public int countMest { get; set; }
+    }
+}
