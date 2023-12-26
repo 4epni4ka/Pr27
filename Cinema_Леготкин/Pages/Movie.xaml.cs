2eee99ff5e8c525466054ae16101cd5aec399a6e
Stepan <legotkinc2006@gmail.com>
2023-12-26 21:31:03 +0500

    Реализовал удобную сортировку данных. Леготкин

--- Cinema_Леготкин/Pages/Movie.xaml.cs
+++ Cinema_Леготкин/Pages/Movie.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Cinema_Леготкин.Classes.Context;
+using DBConnection;
+using MySql.Data.MySqlClient;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -45,6 +48,77 @@ namespace Cinema_Леготкин.Pages
         private void Click_Teatr(object sender, RoutedEventArgs e)
         {
             MainWindow.init.OpenPages(MainWindow.pages.teatr);
+        }
+
+        public bool sortName = false;
+        private void Sort_Name(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.movie ORDER BY movie;";
+            if (sortName)
+            {
+                query = "SELECT * from cinema.movie ORDER BY movie DESC;";
+                sortName = false;
+            }
+            else sortName = true;
+            Sort_Query(query);
+        }
+
+        public bool sortTeatr = false;
+        private void Sort_Teatr(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.movie ORDER BY idTeatr;";
+            if (sortTeatr)
+            {
+                query = "SELECT * from cinema.movie ORDER BY idTeatr DESC;";
+                sortTeatr = false;
+            }
+            else sortTeatr = true;
+            Sort_Query(query);
+        }
+
+        public bool sortDate = false;
+        private void Sort_Date(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.movie ORDER BY time;";
+            if (sortDate)
+            {
+                query = "SELECT * from cinema.movie ORDER BY time DESC;";
+                sortDate = false;
+            }
+            else sortDate = true;
+            Sort_Query(query);
+        }
+
+        public bool sortPrice = false;
+        private void Sort_Price(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.movie ORDER BY price;";
+            if (sortPrice)
+            {
+                query = "SELECT * from cinema.movie ORDER BY price DESC;";
+                sortPrice = false;
+            }
+            else sortPrice = true;
+            Sort_Query(query);
+        }
+        public void Sort_Query(string query)
+        {
+            List<MovieContext> allTMovie = new List<MovieContext>();
+
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader movieQuery = Connection.Query(query, connection);
+            while (movieQuery.Read())
+            {
+                allTMovie.Add(new MovieContext(
+                     movieQuery.GetInt32(0),
+                     movieQuery.GetInt32(1),
+                     movieQuery.GetString(2),
+                     movieQuery.GetDateTime(3),
+                     movieQuery.GetInt32(4)));
+            }
+            MainWindow.AllMovie = allTMovie;
+            Connection.CloseConnection(connection);
+            CreateUI();
         }
     }
 }
--- Cinema_Леготкин/Pages/Teatr.xaml.cs
+++ Cinema_Леготкин/Pages/Teatr.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Cinema_Леготкин.Classes.Context;
+using DBConnection;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,6 +47,63 @@ namespace Cinema_Леготкин.Pages
         private void Click_Movie(object sender, RoutedEventArgs e)
         {
             MainWindow.init.OpenPages(MainWindow.pages.movie);
+        }
+
+        public bool sortName = false;
+        private void SortName(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.teatr ORDER BY name;";
+            if (sortName)
+            {
+                query = "SELECT * from cinema.teatr ORDER BY name DESC;";
+                sortName = false;
+            }
+            else sortName = true;
+            SortQuery(query);
+        }
+
+        public bool sortZal = false;
+        private void SortZal(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.teatr ORDER BY countZal;";
+            if (sortZal)
+            {
+                query = "SELECT * from cinema.teatr ORDER BY countZal DESC;";
+                sortZal = false;
+            }
+            else sortZal = true;
+            SortQuery(query);
+        }
+
+        public bool sortMest = false;
+        private void SortMest(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * from cinema.teatr ORDER BY countMest;";
+            if (sortMest)
+            {
+                query = "SELECT * from cinema.teatr ORDER BY countMest DESC;";
+                sortMest = false;
+            }
+            else sortMest = true;
+            SortQuery(query);
+        }
+        private void SortQuery(string query)
+        {
+            List<TeatrContext> allTeatr = new List<TeatrContext>();
+
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader teatrQuery = Connection.Query(query, connection);
+            while (teatrQuery.Read())
+            {
+                allTeatr.Add(new TeatrContext(
+                    teatrQuery.GetInt32(0),
+                    teatrQuery.GetString(1),
+                    teatrQuery.GetInt32(2),
+                    teatrQuery.GetInt32(3)));
+            }
+            Connection.CloseConnection(connection);
+            MainWindow.AllTeatr = allTeatr;
+            CreateUI();
         }
     }
 }
